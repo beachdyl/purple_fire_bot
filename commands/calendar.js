0393fd9cb7f94494e05d4716fbd506f9eb00a715
Dylan MacAllaster <dylanmac1011@gmail.com>
2021-10-23 21:14:50 -0400

    add multiple embeds to message

--- commands/calendar.js
+++ commands/calendar.js
@@ -2,11 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const csv = require('csv-parser');
 const fs = require('fs');
-let events = [];
-//const embeds = [];
-let title = '';
-let desc = 'yes';
-let _embed;
+const events = [];
+const event_embeds = [];
+
 
 /* for (let i = 0; i < 5; i++) {
 	const embed = new MessageEmbed()
@@ -23,17 +21,25 @@ module.exports = {
 		.setName('calendar')
 		.setDescription('Get a list of upcoming meetings and events!'),
 	async execute(interaction) {
-		fs.createReadStream('google/events.csv')
-			.pipe(csv({headers: false}))
-			.on('data', (data) => events.push(data))
-		await new Promise((resolve) => {setTimeout(resolve, 300)});
-		let _embed = new MessageEmbed()
-					.setColor('#532d8e')
-					// .setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
-					.setTitle(events[0][1])
-					.setDescription(events[0][1])
-					.addField('Description2', '${events.at(i)[2])} ');
-		console.log(events.at(0));
+		await new Promise((resolve) => {
+			fs.createReadStream('google/events.csv')
+				.pipe(csv({ headers: false }))
+				.on('data', (data) => events.push(data))
+				.on('end', () => setTimeout(resolve, 1));
+		});
+
+		for (event in events) {
+			const _embed = new MessageEmbed()
+				.setColor('#532d8e')
+				.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
+				.setTitle(event[1])
+				.setDescription('${event[4]} - ${event[5]')
+				.addField('Location', event[3]);
+			if (event[2]) {
+				_embed.addField('Description:', event[2]);
+			}
+			event_embeds.push(_embed);
+		}
 		const row = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
@@ -41,7 +47,7 @@ module.exports = {
 					.setURL('https://calendar.google.com/calendar/embed?src=5r67hb19jke4qk7jkeftov91f8%40group.calendar.google.com&ctz=America%2FNew_York')
 					.setStyle('LINK'),
 			);
-		await interaction.reply({ephemeral: false, embeds: [_embed], components: [row] });
+		await interaction.reply({ephemeral: false, embeds: event_embeds, components: [row] });
 	},
 };
 
