1f9cd402a376658c6944fde5f20b3f1dcc201b88
Dylan Bowerman <beachdyl@gmail.com>
2021-10-25 01:40:43 -0400

    clean up unused code

--- commands/greeting.js
+++ commands/greeting.js
@@ -17,14 +17,7 @@ module.exports = {
 			.addField('Similarity', 'Robo-Kathryn is a work of fiction. Any similarity to actual persons, living or dead, is purely coincidental.', false)
 			.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
 			.setTimestamp();
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setLabel('Open the Calendar')
-					.setURL('https://calendar.google.com/calendar/embed?src=5r67hb19jke4qk7jkeftov91f8%40group.calendar.google.com&ctz=America%2FNew_York')
-					.setStyle('LINK'),
-			);
-		await interaction.reply({ephemeral: false, embeds: [exampleEmbed], components: [] });
+		await interaction.reply({ephemeral: false, embeds: [exampleEmbed] });
 	},
 };
 
