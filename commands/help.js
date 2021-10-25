1e5124583563a325f4b23599192452b1ec76632c
Dylan Bowerman <beachdyl@gmail.com>
2021-10-25 01:16:06 -0400

    update help text

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
 			.setDescription('Sure thing! Here are the commands I\'m prepared to handle at the moment. Remember, all commands start with a slash (/) typed straight into the message box.')
 			.addField('/calendar [section]', 'Gets you a list with the next 10 events on our calendar, as well as a link to the calendar itself. If *section* is specified, it will filter to only show events hosted by that section (general meetings are always shown).', false)
 			.addField('/meeting', 'If somebody asked for information about a meeting that is on the calendar, you can use this to give them a handy link to it, so they can find the answer.', false)
-			.addField('/announce', 'For club leaders to publish an announcement to an announcement channel.', false)
+			.addField('/announce <section>', 'For club leaders to publish an announcement to the announcement channel of *section*.', false)
 			.addField('/greeting', 'I\'d love to tell you a little more about myself.', false)
 			.addField('/help', 'Bring up this help message again!', false)
 			.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
