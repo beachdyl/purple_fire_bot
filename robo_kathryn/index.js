33440d7ec5b8914e12b54c605a5c2412f455de88
Dylan Bowerman <dbowerman7652@floridapoly.edu>
2021-10-26 00:50:44 -0400

    add error handler function

--- robo_kathryn/index.js
+++ robo_kathryn/index.js
@@ -8,6 +8,23 @@ const { token, clientId, guildId } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+function errHandle(error) {
+	console.error(error);
+	fs.writeFileSync('./error.txt',`${error}`);
+	const errorEmbed = new MessageEmbed()
+			.setColor('#ff0000')
+			.setTitle('Hey there, I\'m Robo-Kathryn!')
+			.setAuthor('Purple Fire Robotics', 'https://i.ibb.co/cDrSdS5/PF-Flame.png', 'https://purplefire.org')
+			.setDescription('I am here to help out with a few of the simple tasks around here. I\'m still learning, so please, have some patience.')
+			.addField('Developer', 'This bot is being developed by <@200316185445793792> and <@581128269584138250>.', true)
+			.addField('Problem?', 'Did I make a mistake? No worries, just tell a Dylan.', true)
+			.addField('Open Source', 'You can find the source code at https://github.com/beachdyl/purple_fire_bot/', false)
+			.addField('Similarity', 'Robo-Kathryn is a work of fiction. Any similarity to actual persons, living or dead, is purely coincidental.', false)
+			.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
+			.setTimestamp();
+	client.channels.cache.get('id').send(`${error}`);
+}
+
 // Register commands from commands directory
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
