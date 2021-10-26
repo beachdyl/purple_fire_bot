191848935965dab6cf058eb463fdc4552fbcc49c
Dylan MacAllaster <dylanmac1011@gmail.com>
2021-10-25 20:44:18 -0400

    Add permissions to crash command so it doesnt get used

--- commands/crash.js
+++ commands/crash.js
@@ -5,6 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('crash')
 		.setDescription('Forces the robot to crash. Please do not use this unless you know what you\'re doing.'),
+	defaultPermission: false,
 	async execute(interaction) {
 		const row = new MessageActionRow()
 			.addComponents(
@@ -12,7 +13,21 @@ module.exports = {
 					.setCustomId('primary')
 					.setLabel('Primary')
 					.setStyle('PRIMARY'),
-			);
+		);
+		const fullPermissions = [
+			{
+				id: '581128269584138250',
+				type: 'USER',
+				permission: true,
+			},
+			{
+				id: '200316185445793792',
+				type: 'USER',
+				permission: true,
+			},
+		];
+		await command.permissions.add({ fullPermissions });
+		//purposefully crash the bot by sending 2 messages, which triggers a bash script to update the bot and restart it
 		await interaction.reply({ content: 'Crashing now! Hopefully, I\'ll be back soon.\n**o7**' });
 		await interaction.reply({ content: '', components: [row] });
 	},
