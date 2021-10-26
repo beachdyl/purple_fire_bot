285ec737c2d439f5420b3a4fce0a055e7a0f4791
Dylan MacAllaster <dylanmac1011@gmail.com>
2021-10-25 21:37:07 -0400

    fix crash perms

--- commands/crash.js
+++ commands/crash.js
@@ -1,11 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton } = require('discord.js');
 
+const fullPermissions = [
+	{
+		id: '581128269584138250',
+		type: 'USER',
+		permission: true,
+	},
+	{
+		id: '200316185445793792',
+		type: 'USER',
+		permission: true,
+	},
+];
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('crash')
 		.setDescription('Forces the robot to crash. Please do not use this unless you know what you\'re doing.')
-		.setDefaultPermission(false),
+		.setDefaultPermission(false)
+		.permissions.add({ fullPermissions }),
 	async execute(interaction) {
 		const row = new MessageActionRow()
 			.addComponents(
@@ -14,19 +28,7 @@ module.exports = {
 					.setLabel('Primary')
 					.setStyle('PRIMARY'),
 		);
-		const fullPermissions = [
-			{
-				id: '581128269584138250',
-				type: 'USER',
-				permission: true,
-			},
-			{
-				id: '200316185445793792',
-				type: 'USER',
-				permission: true,
-			},
-		];
-		await permissions.add({ fullPermissions });
+
 		//purposefully crash the bot by sending 2 messages, which triggers a bash script to update the bot and restart it
 		await interaction.reply({ content: 'Crashing now! Hopefully, I\'ll be back soon.\n**o7**' });
 		await interaction.reply({ content: '', components: [row] });
