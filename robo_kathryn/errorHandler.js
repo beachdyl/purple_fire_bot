239d59cb8c1dbca6a7b9b6d11e3decab2e0331ca
Dylan Bowerman <dbowerman7652@floridapoly.edu>
2021-10-26 20:16:34 -0400

    redo client creation

--- robo_kathryn/errorHandler.js
+++ robo_kathryn/errorHandler.js
@@ -4,6 +4,11 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 //errHandle function
 var errHandle = function(error, client) {
+	// Create a new client instance
+	const { Client, Collection, Intents } = require('discord.js');
+	const client2 = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+	//Log error in console and files
 	console.error(error);
 	fs.writeFileSync('./error.txt',`${error}`);
 
@@ -15,7 +20,7 @@ var errHandle = function(error, client) {
 			.addField('Error', `${error}`, false)
 			.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
 			.setTimestamp();
-	client.channels.cache.get('770464638881497089').send({embeds: [errorEmbed] });
+	client2.channels.cache.get('770464638881497089').send({embeds: [errorEmbed] });
 
 };
 
