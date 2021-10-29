fbc5804bdc58b68a6a52e194562e1b4b3256c75d
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-10-29 19:37:27 -0400

    adapt restart to ahave exec for restart script later

--- robo_kathryn/commands/restart.js
+++ robo_kathryn/commands/restart.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton } = require('discord.js');
+const { exec } = require('child_process');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,13 +8,7 @@ module.exports = {
 		.setDescription('Restarts the robot, and re-caches events and commands.')
 		.setDefaultPermission(false),
 	async execute(interaction) {
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
+		exec('xxxxx')
 		//purposefully crash the bot by sending 2 messages, which then triggers a script to update the bot and restart it
 		await interaction.reply({ content: 'Restarting now! Hopefully, I\'ll be back soon.\n**o7**' });
 		await interaction.reply({ content: '', components: [row] });
