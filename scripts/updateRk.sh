5ace6fd99f1b89d8e8c177e158342614d08af867
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-04 22:03:53 -0400

    fix bugs in updateRk.sh script

--- scripts/updateRk.sh
+++ scripts/updateRk.sh
@@ -2,7 +2,8 @@
 # updateRk.sh
 # git update robokathryn with provided branch. does not kill existing processes.
 
-if [ $# = 0 ] then
+if [ $# -eq 0 ]
+then
 	echo "You must specify a branch to update with"
 	echo "To update using current branch and run, try run.sh"
 else
