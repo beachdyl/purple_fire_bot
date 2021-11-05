eda7a64df80b6f2301cdff45ee4e5307e233b5cf
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-05 00:03:36 -0400

    remove killRestart.sh

--- killRestart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# killRestart.sh
-# kill existing robo-kathryns and then restart her
-
-pid=`ps aux | grep node | grep -v grep | grep -v root | awk '{print $2}'`
-kill $pid
-/srv/shared/rk/files/scripts/run.bash || /srv/shared/rk/files/run.bash
