e29373e3333df57e52815a94e90f388b6a83f180
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-04 22:45:50 -0400

    fix merge errors

--- robo_kathryn/commands/restart.js
+++ robo_kathryn/commands/restart.js
@@ -1,21 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton } = require('discord.js');
+const { exec } = require('child_process');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('restart')
 		.setDescription('Restarts the robot, and re-caches events and commands.')
-		.setDefaultPermission(false),
+		.setDefaultPermission(true),
 	async execute(interaction) {
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
-		//purposefully crash the bot by sending 2 messages, which then triggers a script to update the bot and restart it
 		await interaction.reply({ content: 'Restarting now! Hopefully, I\'ll be back soon.\n**o7**' });
-		await interaction.reply({ content: '', components: [row] });
+
+		script = exec('cd ..; scripts/killRestart.sh || ./killRestart.sh');
+		// what to do for data coming from the standard out
+		script.stdout.on('data', function(data){
+			console.log(data.toString());
+		});
+		// what to do with data coming from the standard error
+		script.stderr.on('data', function(data){
+			console.error(data.toString());
+		});
+		// what to do when the command is done
+		script.on('exit', function(code){
+			console.log('program ended with code: ' + code);
+		});
 	},
 };
